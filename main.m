b9dfc13c2e38702a8678403cf48627715fe106b6
GuRX <GuRX@DESKTOP-FEKGT0T>
2025-04-09 21:51:33 +0800

    加入优选对的生成

--- main.m
+++ main.m
@@ -1,7 +1,10 @@
 clc; clear; close all;
 
-load("parameters.mat", "est_PN_code");
+load(".\Data\parameters.mat", "est_PN_code", "pairs");
+
+% [num, pairs] = getoptpairs(L); % obtain all optimal pairs of L-th order
+% fprintf('obtained %d optimal pairs of %d order.\n', num, L);
 
 tic
-[reseq, poly, state] = persc(est_PN_code);
+[reseq, poly, state] = persc(est_PN_code, pairs);
 toc
