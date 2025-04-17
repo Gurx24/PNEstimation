98e417f5ae0458ba02634e19342de328223864cb
GuRX <17633837037@163.com>
2025-04-17 17:11:26 +0800

    多个多项式可自行设置多个状态，提升函数扩展性

--- src/msgen.m
+++ src/msgen.m
@@ -1,8 +1,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%% msgen.m %%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% This function is used to generate m sequence.
+% This function is used to generate m sequence 
+% in the form of a row vector.
 % Multiple m sequences can be generated based on 
-% multiple generator polynomials.
+% multiple generator polynomials and initial states.
 %
 % date : 2025.4.2  GuRX
 %
@@ -11,26 +12,29 @@
 % *********************************************************
 % polys : octal generator polynomial vector of m sequence
 % L     : order of m sequence
+% inist : octal initial state vector of m sequence
 % mseq  : generated m sequence
 % *********************************************************
 
 function mseq = msgen(polys, L, inist)
 
 switch nargin
-    case 2
-        state = [zeros(1, L-1) 1];      % default initial state
+    case 2  
+        state = repmat([zeros(1, L-1) 1], size(polys, 2), 1);
+                                      % default initial state
     case 3
-        state = inist;                  % specified initial state
+        state = dec2bin(base2dec(num2str(inist.'), 8), L)-'0';
+                                      % specified initial state
 end
 
 N     = 2^L - 1;                      % length of m sequence
 mseq  = zeros(size(polys,2), N);      % initialization of m sequence
-bpair = dec2bin(base2dec(num2str(polys.'), 8))-'0';
+bpair = dec2bin(base2dec(num2str(polys.'), 8), L+1)-'0';
 
 for i = 1:size(bpair, 1)
     mSeqGen = comm.PNSequence( ...
         'Polynomial', bpair(i, :), ...
-        'InitialConditions', state, ...
+        'InitialConditions', state(i, :), ...
         'SamplesPerFrame', N);
     mseq(i, :) = mSeqGen();
 end
